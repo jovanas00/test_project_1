f044d3a6c4643939c35928798e333bcea00af97b
Jovana <jovanastojanovic394@gmail.com>
2024-01-10 15:30:41 +0100

    Izmena 3

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+// localStorage.setItem('isLogedIn', false);
+// localStorage.setItem('email', 'olivia@untitledui.com');
+// localStorage.setItem('password', 'olivia123');
+
+// const logInBtn = document.querySelector('.log-in');
+// logInBtn.addEventListener('click', function() {
+//     check();
+// })
+
+// function check() {
+//     const storedEmail = localStorage.getItem('email');
+//     const storedPassword = localStorage.getItem('password');
+//     const isLogedIn = localStorage.getItem('isLogedIn');
+
+//     const userEmail = document.querySelector('.user-email');
+//     const userPassword = document.querySelector('.user-password');
+
+//     if(userEmail.value !== storedEmail || userPassword.value !== storedPassword) {
+//         alert("Netacni podaci");
+//     }
+//     else if(isLogedIn){
+//         alert("Vec ste ulogovani!");
+//     }
+//     else
+//     {
+//         localStorage.getItem('isLogedIn', true);
+//         window.location = 'index.html';
+//     }
+// }
+
+const logOut = document.querySelector('.sidebar-button');
+logOut.addEventListener('click', function() {
+    localStorage.setItem('isLogedIn', false);
+    window.location = 'login.html';
+});
+
 const addBtn = document.querySelector('.add-clients-btn');
 const modal = document.querySelector('.modal-div');
 const deleteClientModal = document.querySelector('.delete-client-modal-div');
@@ -11,6 +47,14 @@ addBtn.addEventListener('click', function() {
 const closeBtnModal = modal.querySelector('img');
 
 closeBtnModal.addEventListener('click', function() {
+    modal.querySelector('.name-input').value = "";
+    modal.querySelector('.email-input').value = "";
+    modal.querySelector('.phone-input').value = "";
+    modal.querySelector('.company-input').value = "";
+    modal.querySelector('.country-input').value = "";
+    modal.querySelector('.zip-input').value = "";
+    modal.querySelector('.address-input').value = "";
+    modal.querySelector('.apt-input').value = "";
     modal.style.display = 'none';
 });
 
@@ -31,10 +75,13 @@ addBtnModal.addEventListener('click', function() {
     const email = modal.querySelector('.email-input');
     const phone = modal.querySelector('.phone-input');
     const company = modal.querySelector('.company-input');
+    const companyValue = company.value;
     const country = modal.querySelector('.country-input');
     const zip = modal.querySelector('.zip-input');
+    const zipValue = zip.value;
     const address = modal.querySelector('.address-input');
     const apt = modal.querySelector('.apt-input');
+    const aptValue = apt.value;
     // if(name.value === "" || email.value === "" || phone.value === "" || company.value === "" || country.value === "" || 
     // zip.value === "" || address.value === "" || apt.value === "") {
     //     alert("Niste uneli sve podatke!");
@@ -50,6 +97,7 @@ addBtnModal.addEventListener('click', function() {
     noviDiv.appendChild(nameEmail);
 
     const nameDiv = document.createElement('div');
+    nameDiv.classList.add('name-column');
     nameDiv.innerText = name.value;
     const nameObject = {id: noviDiv.id, name: nameDiv.innerText};
     names.push(nameObject);
@@ -82,7 +130,7 @@ addBtnModal.addEventListener('click', function() {
     link.innerText = "False";
     link.style.width = "40px"
     link.style.borderRadius = "0.5rem";
-    if(link.innerText === "True")
+    if(link.innerText === "True" || link.innerText === "Active")
     {
         link.style.backgroundColor = "#ECFDF3";
         link.style.color = "#027A48";
@@ -107,6 +155,15 @@ addBtnModal.addEventListener('click', function() {
     editBtn.setAttribute('src', 'images/modal/edit.png');
     buttonsDiv.appendChild(editBtn);
 
+    deleteBtn.addEventListener('click', function() {
+        deleteClientModal.id = noviDiv.id;
+        deleteClientModal.style.display = "flex";
+    });
+
+    editBtn.addEventListener('click', function() {
+        openEditModal(noviDiv.id, nameDiv.innerText, emailDiv.innerText, phoneDiv.innerText, companyValue, countryDiv.innerText, zipValue, addressDiv.innerText, aptValue);
+    })
+
     name.value = ""; 
     email.value = ""; 
     phone.value = ""; 
@@ -122,12 +179,42 @@ addBtnModal.addEventListener('click', function() {
     let num = parseInt(numberOfClients.innerText);
     num++;
     numberOfClients.innerText = num.toString();
-
-    deleteBtn.addEventListener('click', function() {
-        deleteClientModal.id = noviDiv.id;
-        deleteClientModal.style.display = "flex";
-    });
 });
+
+function openEditModal(rowId, name, email, phone, company, country, zip, address, apt) {
+    modal.style.display = 'flex';
+
+    modal.querySelector('.name-input').value = name;
+    modal.querySelector('.email-input').value = email;
+    modal.querySelector('.phone-input').value = phone;
+    modal.querySelector('.company-input').value = company;
+    modal.querySelector('.country-input').value = country;
+    modal.querySelector('.zip-input').value = zip;
+    modal.querySelector('.address-input').value = address;
+    modal.querySelector('.apt-input').value = apt;
+
+    const currentRow = document.getElementById(rowId);
+    const rowCells = currentRow.cells;
+    console.log(rowCells);
+    
+    modal.querySelector('.add-clients-btn').addEventListener('click', function() {
+        rowCells[1] = modal.querySelector('.name-input').value;
+        // rowCells[1] =  modal.querySelector('.email-input').value;
+        rowCells[2] =  modal.querySelector('.phone-input').value;
+        // rowCells[1] =  modal.querySelector('.email-input').value;
+
+        modal.querySelector('.name-input').value = "";
+        modal.querySelector('.email-input').value = "";
+        modal.querySelector('.phone-input').value = "";
+        modal.querySelector('.company-input').value = "";
+        modal.querySelector('.country-input').value = "";
+        modal.querySelector('.zip-input').value = "";
+        modal.querySelector('.address-input').value = "";
+        modal.querySelector('.apt-input').value = "";
+        
+        modal.style.display = 'none';
+    });
+}
 
 const clientInfoBtn = document.querySelector(".client-info-button");
 const pageInfo = document.querySelector(".page-info");
@@ -166,7 +253,6 @@ const deleteClientDeleteBtn = document.querySelector('.delete-client-delete-btn'
 deleteClientDeleteBtn.addEventListener('click', function() {
     const tableDiv = document.querySelector('.table-rows');
     let tableRow = document.getElementById(deleteClientModal.id);
-    // console.log(tableRow);
     tableDiv.removeChild(tableRow);
     
     deleteClientModal.style.display = "none";
@@ -181,21 +267,178 @@ const inputTableDiv = document.querySelector('.table-rows');
 
 const pageSearch = document.querySelector('.page-search');
 pageSearch.addEventListener('input', function() {
-    
-    const tableChildren = inputTableDiv.innerHTML;
-    console.log(tableChildren);
     let filter = pageSearch.value.toUpperCase();
-    names.forEach(n => {
-        if(n.name.toUpperCase() === filter)
+    const tableRows = document.querySelectorAll('.table-rows .table-row');
+    tableRows.forEach(row => {
+        const nameColumn = row.querySelector('.name-column');
+        if(nameColumn)
         {
-            let row = document.getElementById(`${n.id}`);
-            // inputTableDiv.replaceChildren();
-            inputTableDiv.appendChild(row);
-        }
-        else
-        {
-            inputTableDiv.replaceChildren();
-            inputTableDiv.innerHTML = tableChildren;
+            const nameText = nameColumn.textContent.toUpperCase();
+            if(nameText.includes(filter))
+                row.style.display = '';
+            else
+                row.style.display = 'none';
         }
     });
+
+    if(!filter) {
+        tableRows.forEach(row => {
+            row.style.display = '';
+        })
+    }
+});
+
+const newLinkBtn = document.querySelector('.new-link-btn');
+const newLinkModal = document.querySelector('.new-link-modal-div');
+const accessListModal = document.querySelector('.access-list-modal-div');
+const closeBtnAccessList = accessListModal.querySelector('img');
+
+newLinkBtn.addEventListener('click', function() {
+    newLinkModal.style.display = 'flex';
+});
+
+const closeBtn = newLinkModal.querySelector('img');
+
+closeBtn.addEventListener('click', function() {
+    newLinkModal.style.display = 'none';
+});
+
+const sendLinkBtn = newLinkModal.querySelector('.send-link-btn');
+let linkBrojac = 0;
+
+sendLinkBtn.addEventListener('click', function() {
+    linkBrojac++;
+
+    const tableDiv = document.querySelector('.table-rows-links');
+
+    let noviID = '#'+linkBrojac;
+    const noviDiv = document.createElement('tr');
+    noviDiv.setAttribute('class', 'table-row');
+    noviDiv.setAttribute('id', linkBrojac);
+    tableDiv.appendChild(noviDiv);
+
+    const idDiv = document.createElement('td');
+    idDiv.innerText = noviID;
+    idDiv.classList.add('table-cell');
+    noviDiv.appendChild(idDiv);
+
+    const issuedBy = document.createElement('td');
+    issuedBy.classList.add('table-cell');
+    noviDiv.appendChild(issuedBy);
+    issuedBy.innerText = "John Malkovich";
+
+    const issueDate = document.createElement('td');
+    issueDate.classList.add('table-cell');
+    noviDiv.appendChild(issueDate);
+    issueDate.innerText = "22.08.2023";
+
+    const radioGroup1 = newLinkModal.querySelector('.radio-group1');
+    console.log(radioGroup1);
+
+    const timeLimitation = newLinkModal.querySelector('.time-limitation'); 
+    const accessLimitation = newLinkModal.querySelector('.access-limitation');
+    const expirationUnlimited = newLinkModal.querySelector('.expiration-unlimited');
+    const date = document.querySelector('.select-date'); 
+    const dateValue = date.value;
+    const time = document.querySelector('.select-time-input');
+
+    const expiration = document.createElement('td');
+    expiration.classList.add('table-cell');
+    noviDiv.appendChild(expiration);
+
+    if(timeLimitation.checked)
+    {
+        const date1 = new Date(dateValue);
+        const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+        expiration.innerText = `${date1.getDay()}.${months[date1.getMonth()]}.${date1.getFullYear()}.`;
+    }
+    if(accessLimitation.checked)
+        expiration.innerText = "Access limitation";
+    if(expirationUnlimited.checked)
+        expiration.innerText = "Unlimited";
+
+    const projectAccessLimitation = newLinkModal.querySelector('.project-access-limitation');
+    // console.log(projectAccessLimitation);
+    const onlySelected = newLinkModal.querySelector('.only-selected');
+    const selectTime = newLinkModal.querySelector('.select-time'); 
+
+    const projectAccess = document.createElement('td');
+    projectAccess.classList.add('table-cell');
+    noviDiv.appendChild(projectAccess);
+
+    if(projectAccessLimitation.checked)
+        projectAccess.innerText = "Access limitation";
+    if(onlySelected.checked)
+    {
+        
+        projectAccess.innerText = selectTime.value;
+        projectAccess.style.textDecoration = "underline";
+        projectAccess.style.color = "#6941C6";
+        projectAccess.style.cursor = "pointer";
+        projectAccess.addEventListener('click', function() {
+            accessListModal.style.display = 'flex';
+        });
+    }
+
+    const downloading = document.createElement('td');
+    downloading.classList.add('table-cell');
+    noviDiv.appendChild(downloading);
+
+    const link = document.createElement('div');
+    link.innerText = "False";
+    link.style.width = "60px"
+    link.style.borderRadius = "0.5rem";
+    downloading.appendChild(link);
+
+    link.innerText = "Allowed";
+
+    const action = document.createElement('td');
+    action.classList.add('table-cell');
+    noviDiv.appendChild(action);
+    action.style.display = "flex";
+    action.style.columnGap = "2rem";
+
+    const edit = document.createElement('img');
+    edit.src = "images/edit_link.png";
+    action.appendChild(edit);
+
+    if(downloading.innerText === "Allowed")
+    {
+        link.style.backgroundColor = "#ECFDF3";
+        const allowed = document.createElement('img');
+        allowed.src = "images/pause-circle.png";
+        action.appendChild(allowed);
+    }
+    if(downloading.innerText === "Paused")
+    {
+        link.style.backgroundColor = "#FFFAEB";
+        link.style.color = "#FFFAEB";
+        const paused = document.createElement('img');
+        paused.src = "images/play-circle.png";
+        action.appendChild(paused);
+    }
+    if(downloading.innerText === "Denied")
+    {
+        link.style.backgroundColor = "#FEF3F2";
+        link.style.color = "#B42318";
+    }
+
+    const deleteLink = document.createElement('img');
+    deleteLink.src = "images/trash-2.png";
+    action.appendChild(deleteLink);
+
+    let numberOfLinks = document.querySelector(".number-of-links");
+    let num = parseInt(numberOfLinks.innerText);
+    num++;
+    numberOfLinks.innerText = num.toString();
+
+    date.value = "";
+    time.value = "";
+    selectTime.value = "";
+
+    newLinkModal.style.display = 'none';
+});
+
+closeBtnAccessList.addEventListener('click', function() {
+    accessListModal.style.display = 'none';
 });
